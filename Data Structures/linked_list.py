2aec4262bd42ee8cf8797bdb99519c44eca85e3d
Achint Bhat <abhat29@asu.edu>
2022-12-28 19:06:52 -0700

    update

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -1,7 +1,14 @@
 class Node:
-    def __init__(self, val) -> None:
+
+    def __init__(self, val=0, next=None) -> None:
         self.data = val
-        self.next = None
+        self.next = next
+
+    def __gt__(self, n):
+        return self.data > n.data
+
+    def __repr__(self):
+        return str(self.data) + ' -> ' + str(self.next.data) if self.next else 'X'
 
 
 class LinkedList:
@@ -17,7 +24,7 @@ class LinkedList:
         while temp.next:
             temp = temp.next
             res.append(temp.data)
-        return '->'.join([str(x) for x in res])
+        return ' -> '.join([str(x) for x in res])
 
     def insert_end(self, node):
         temp = self.root
@@ -29,6 +36,54 @@ class LinkedList:
         node.next = self.root.next
         self.root.next = node
 
+    def get_node_at_pos(self, pos):
+        temp = self.root
+        for _ in range(pos+1):
+            temp = temp.next
+        return temp
+
+    def count_nodes(self):
+        count = 0
+        temp = self.root
+        while temp.next:
+            temp = temp.next
+            count += 1
+        return count
+
+    def get_middle(self):
+        count = self.count_nodes()
+        pos = count//2
+        if count/2 - count // 2:
+            pos += 1
+        print(pos)
+        return self.get_node_at_pos(pos)
+
+    def middle(self):
+        slow = fast = self.root
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow.next
+
+    def is_sorted(self):
+        temp = self.root.next
+        while temp.next:
+            if temp > temp.next:
+                return False
+            temp = temp.next
+        return True
+
 
 if __name__ == "__main__":
     l1 = LinkedList()
+    for i in range(0, 12):
+        l1.insert_end(Node(i))
+
+    print(l1)
+    print(l1.count_nodes())
+    print(l1.get_node_at_pos(1))
+    print(l1.get_middle())
+    print(l1.middle())
+    print(l1.is_sorted())
+    l1.insert_beginning(Node(999))
+    print(l1.is_sorted())
